fea7a328660fd916c3838578354f7e5f9f7b3aa4
Halkin David <halkin.david@gmail.com>
2021-08-23 17:10:26 +0300

    sdf

--- app/js/main.js
+++ app/js/main.js
@@ -16,4 +16,20 @@ $(document).ready(function(){
         $(this).parent().toggleClass("opened");
         $(this).parent().next(".hide_section_js").slideToggle("fast");
     });
+
+    // go  to top
+    var btn = $('.go_top');
+
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    });
+
+    btn.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop:0}, '300');
+    });
 });
